a55c4f787694e09b9f8ceb0aef095301b9a1a5a2
TLarsh <adeyemisam2015@gmail.com>
2025-09-03 08:30:14 +0100

    updated response.ts and errror handler.ts

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,13 +1,29 @@
-import { Request, Response, NextFunction } from "express";
+// import { Request, Response, NextFunction } from "express";
 
-export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error("Error:", err.message);
+// export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+//     console.error("Error:", err.message);
 
-    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+//     const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+//     const message = err.message || "Internal Server Error";
+
+//     res.status(statusCode).json({
+//         success: false,
+//         message,
+//     });
+// };
+
+
+
+import {Request, Response, NextFunction} from "express"
+
+export const errorHandler = (err:Error, req:Request, res:Response, next:NextFunction) => {
+    console.error("Error:", err);
+
+    const statusCode = res.statusCode || 500;
     const message = err.message || "Internal Server Error";
 
     res.status(statusCode).json({
         success: false,
-        message,
-    });
-};
+        message
+    })
+}
---
+++ src/utils/response.ts
@@ -0,0 +1,16 @@
+import { Response } from "express";
+
+export const successResponse = (res:Response, data:any, message="Success", code=200) => {
+    return res.status(code).json({
+        success: true,
+        message,
+        data,
+    });
+};
+
+export const errorMessage = (res:Response, message= "Something went wrong", code=500) => {
+    return res.status(code).json({
+        success: false,
+        message,
+    })
+}
