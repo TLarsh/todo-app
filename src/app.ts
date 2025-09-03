9612d03d8ccef7af305a309d9dc68e95aabde7e9
TLarsh <adeyemisam2015@gmail.com>
2025-09-03 12:00:29 +0100

    updated authRoute, apps.ts, authService.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv'
 import connectDB from './config/db';
+import authRoutes from './routes/authRoutes';
 
 dotenv.config();
 connectDB();
@@ -9,3 +10,5 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes);
+
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,3 +1,19 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User, { IUser } from "../models/User";
+
+export const registerUser = async (username: string, email: string, password: string): Promise<{ token: string; user: IUser }> => {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+        throw new Error("User already exists");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+
+    return { token, user: newUser };
+};
+
+// export { registerUser };
