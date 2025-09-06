3395ceb87ac75b80404aaa7a26e58c3836ba6617
TLarsh <adeyemisam2015@gmail.com>
2025-09-06 10:56:59 +0100

    login authorizition

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { registerUser } from "../services/authService";
+import { registerUser, loginUser } from "../services/authService";
 import { successResponse } from "../utils/response";
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
@@ -8,6 +8,16 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
         const { token, user } = await registerUser(username, email, password);
         return successResponse(res, { token, user }, "User Registered.", 201);
     } catch (err) {
+        next(err);
+    }
+};
+
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { email, password } = req.body;
+        const { token, user} = await loginUser(email, password);
+        res.json({success:true, token, user})
+    } catch ( err ) {
         next(err);
     }
 };
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,19 +1,53 @@
-import bcrypt from "bcrypt";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+// import User, { IUser } from "../models/User";
+
+// export const registerUser = async (username: string, email: string, password: string): Promise<{ token: string; user: IUser }> => {
+//     const existingUser = await User.findOne({ email });
+//     if (existingUser) {
+//         throw new Error("User already exists");
+//     }
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     const newUser = new User({ username, email, password: hashedPassword });
+//     await newUser.save();
+
+//     const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
+
+//     return { token, user: newUser };
+// };
+
+
+import bcrypt from 'bcrypt';
 import jwt from "jsonwebtoken";
-import User, { IUser } from "../models/User";
+import User, {IUser} from "../models/User";
 
-export const registerUser = async (username: string, email: string, password: string): Promise<{ token: string; user: IUser }> => {
-    const existingUser = await User.findOne({ email });
+export const registerUser = async (username:string, email:string, paassword:string): Promise<{token:string; user:IUser}> => {
+    const existingUser = await User.findOne({email});
     if (existingUser) {
-        throw new Error("User already exists");
+        throw new Error("User already exist");
     }
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
-    await newUser.save();
+    const hashedPassword = await bcrypt.hash(paassword, 10);
+    const user = await new User({username,email,password:hashedPassword});
+    await user.save();
+    const token = jwt.sign({id:user._id}, process.env.JWT_SERCRET as string, {expiresIn:"1h"});
 
-    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET as string, { expiresIn: '1h' });
-
-    return { token, user: newUser };
+    return {token, user:user};
 };
 
-// export { registerUser };
+export const loginUser= async(email:string, password:string) => {
+    const user = await User.findOne({email});
+    if (!user) {
+        const err:any = new Error("Invalid Credentials");
+        err.statusCode = 401;
+        throw err;
+    };
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        const err:any = new Error("Invalid Credentials");
+        err.statusCode = 401;
+        throw err;
+    }
+
+    const token = jwt.sign({id:user.id}, process.env.JWT_SECRET as string);
+    return {token, user};
+}
