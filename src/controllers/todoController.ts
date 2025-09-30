2d0fea440d2f51151bc4986d68ea38f31d89b522
TLarsh <adeyemisam2015@gmail.com>
2025-09-30 15:55:52 +0100

    updated todo controller

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -10,3 +10,32 @@ export const create = async(req:any, res:Response) => {
         res.status(400).json({success:false, msg:err.message})
     }
 };
+
+export const list = async(req:any, res:Response) => {
+    try {
+        const todos = await getTodos(req.user);
+        res.status(200).json({succcess:true, todos});
+    } catch (err:any) {
+        res.status(400).json({success:false, msg:err.message});
+    }
+};
+
+export const update = async(req:any, res:Response) => {
+    try {
+        const todo = await updateTodo( req.user, req.params.id, req.body );
+        if (!todo) return res.status(404).json({ success:false, msg:"Todo not found" });
+        res.json({ success:true, todo})
+    } catch (err:any) {
+        res.status(400).json({ success:false, msg:err.message});
+    }
+};
+
+export const remove = async (req: any, res: Response) => {
+  try {
+    const todo = await deleteTodo(req.user, req.params.id);
+    if (!todo) return res.status(404).json({ success: false, msg: "Todo not found" });
+    res.json({ success: true, msg: "Todo deleted" });
+  } catch (err: any) {
+    res.status(400).json({ success: false, msg: err.message });
+  }
+};
